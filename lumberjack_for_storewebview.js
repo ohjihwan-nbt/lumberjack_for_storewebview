984adb616b28f820a9fd7a00655b1beb155749b8
ohjihwan-nbt <oh.jihwan@nbt.com>
2018-09-27 18:37:56 +0900

    수정 : info.pageName 강제 추가
    - 현재 eventName 자체로는 kibana 검색이 어려운 문제가 있어 별도의 키 생성 처리

--- lumberjack_for_storewebview.js
+++ lumberjack_for_storewebview.js
@@ -15677,6 +15677,7 @@ exports.default = {
     if(!info) info = {};
     // kibana에서 스테이지 환경에서의 로그는 제거하고 확인할 수 있도록 info가 무조건 작성되도록 한다.
     info.isStage = this.isStage;
+    info.pageName = String(eventName).split('::')[0].replace('webview-', '') || null
 
     var promiseList = [this.getSession(), this.getDeviceInfo(), this.getAppInfo(), this.getUserInfo()];
     var config = { headers: { 'Content-Type': 'application/vnd.kafka.json.v1+json' } };
